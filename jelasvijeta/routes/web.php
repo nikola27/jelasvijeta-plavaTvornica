46f6a1ae2e58ad2b9f7e1e6e2438222c752bcf4e
nikola27 <nikolica.saric@hotmail.com>
2019-03-19 16:29:40 +0100

    update

--- jelasvijeta/routes/web.php
+++ jelasvijeta/routes/web.php
@@ -9,8 +9,17 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
+
+
+Route::get('/meal', function () {
+    return view('meals.meal');
+});
+
+
+
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::resource('meal', 'MealsController');
+Route::get('/', 'MealsController@index');
+
